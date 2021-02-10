6c1f6a3081a7e8018361b1f6ba62e89856e982c3
redmejia <preynaldorod@gmail.com>
2021-02-09 16:40:07 -0800

    adding cors

---
+++ cors/cors.go
@@ -0,0 +1,13 @@
+package cors
+
+import "net/http"
+
+func Cors(next http.Handler) http.Handler {
+	cors := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PATCH, PUT, DELETE, OPTIONS")
+		next.ServeHTTP(w, r)
+	}
+
+}
