2ad8a6fde5508945c218f7340a61a0404c4a0a16
redmejia <preynaldorod@gmail.com>
2021-04-03 13:31:09 -0700

    small changes

--- product/product.go
+++ product/product.go
@@ -13,9 +13,10 @@ import (
 
 // http://localhost:8080/v1/product?cat=mens-boots&pro-id=1
 func HandleProducts(w http.ResponseWriter, r *http.Request) {
+	var product dbutils.Product
 	rQuery := r.URL.Query()
 	categorie := rQuery["cat"][0]
-	// proId := rQuery["pro-id"][0]
+	proId := rQuery["pro-id"][0]
 	db, err := connection.Dbconn()
 	if err != nil {
 		log.Println("ERRO ", err)
@@ -25,34 +26,33 @@ func HandleProducts(w http.ResponseWriter, r *http.Request) {
 	switch categorie {
 	case queries.MensBoots:
 		// concatinating proId to the querry fix error
-		var product dbutils.Product
-		query := `SELECT * FROM boots_mens`
-		data, err := dbutils.Retrive(db, product, query)
+		query := `SELECT * FROM boots_mens WHERE pro_id = $1`
+		data, err := dbutils.Retrive(db, product, query, proId)
 		if err != nil {
 			log.Println("ERRO ", err)
 		}
 		json.NewEncoder(w).Encode(data)
-	// case queries.MensSport:
-	// 	query := `SELECT * FROM athletic WHERE pro_id = ` + proId
-	// 	product, err := dbutils.Retrive(db, query)
-	// 	if err != nil {
-	// 		log.Println("ERRO ", err)
-	// 	}
-	// 	json.NewEncoder(w).Encode(product)
-	// case queries.WomensBoots:
-	// 	query := `SELECT * FROM boots_womens WHERE pro_id = ` + proId
-	// 	product, err := dbutils.Retrive(db, query)
-	// 	if err != nil {
-	// 		log.Println("ERRO ", err)
-	// 	}
-	// 	json.NewEncoder(w).Encode(product)
-	// case queries.Heels:
-	// 	query := `SELECT * FROM heels WHERE pro_id = ` + proId
-	// 	product, err := dbutils.Retrive(db, query)
-	// 	if err != nil {
-	// 		log.Println("ERRO ", err)
-	// 	}
-	// 	json.NewEncoder(w).Encode(product)
+	case queries.MensSport:
+		query := `SELECT * FROM athletic WHERE pro_id = $1`
+		product, err := dbutils.Retrive(db, product, query, proId)
+		if err != nil {
+			log.Println("ERRO ", err)
+		}
+		json.NewEncoder(w).Encode(product)
+	case queries.WomensBoots:
+		query := `SELECT * FROM boots_womens WHERE pro_id = $1`
+		product, err := dbutils.Retrive(db, product, query, proId)
+		if err != nil {
+			log.Println("ERRO ", err)
+		}
+		json.NewEncoder(w).Encode(product)
+	case queries.Heels:
+		query := `SELECT * FROM heels WHERE pro_id = $1`
+		product, err := dbutils.Retrive(db, product, query, proId)
+		if err != nil {
+			log.Println("ERRO ", err)
+		}
+		json.NewEncoder(w).Encode(product)
 	default:
 		http.Error(w, "Oooops Somethig when wrong. :'( ", http.StatusInternalServerError)
 		return
