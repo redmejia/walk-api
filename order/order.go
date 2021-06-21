40e6a1e68d4fac3ddc2db0055372336bb1895809
redmejia <preynaldorod@gmail.com>
2021-06-21 01:18:08 -0700

    order status

--- order/order.go
+++ order/order.go
@@ -9,9 +9,9 @@ import (
 	"github.com/redmejia/dbutils"
 )
 
-type Msg struct {
-	Msq    string `json:"msg"`
-	Recive bool   `json:"recive"`
+type PurchaseStatus struct {
+	Status          string `json:"status"`
+	TransactionCode uint8  `json:"transaction_code"` // 00 error card num or cv not valid, 02 ok,  05 not enough to compleate 0.0 balance or purchase is grather than amount
 }
 
 func HandleOrder(w http.ResponseWriter, r *http.Request) {
@@ -25,28 +25,30 @@ func HandleOrder(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Println("recived ", order)
 		var nOrder = struct {
-			FirstName  string `json:"first_name"`
-			LastName   string `json:"last_name"`
-			CardNumber string `json:"card_number"`
-			CvNumber   uint8  `json:"cv_number"`
+			CardNumber     string  `json:"card_number"`
+			CvNumber       uint8   `json:"cv_number"`
+			PurchaseAmount float32 `json:"purchase_amount"`
 		}{
-			FirstName:  order.FirstName,
-			LastName:   order.LastName,
-			CardNumber: order.CardNumber,
-			CvNumber:   order.CvNumber,
+			CardNumber:     order.CardNumber,
+			CvNumber:       order.CvNumber,
+			PurchaseAmount: order.Total,
 		}
 		jdata, err := json.Marshal(&nOrder)
 		if err != nil {
 			fmt.Println("err ", err)
 		}
-		resp, err := http.Post("http://192.168.1.104:8082/order", "application/json", bytes.NewBuffer(jdata))
+		// request to middle serv
+		resp, err := http.Post("http://127.0.0.1:8082/order", "application/json", bytes.NewBuffer(jdata))
 		if err != nil {
 			fmt.Println("Erro resp ", err)
+			return
 		}
 		defer resp.Body.Close()
-		var msg Msg
-		json.NewDecoder(resp.Body).Decode(&msg)
-		fmt.Println("youuu ", msg)
+		fmt.Println("handle order ")
+		var status PurchaseStatus
+		json.NewDecoder(resp.Body).Decode(&status)
+		fmt.Println("handleorder status, ", status.Status)
+		// fmt.Println("youuu ", msg)
 		// _, err = dbutils.NewOrder(order.ProID, order.Name, order.Color, order.Size, order.Total)
 		// if err != nil {
 		// 	fmt.Println("ERROR INSERT ", err)
